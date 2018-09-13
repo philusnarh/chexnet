bef2c5b1e4c3c0c50b3f5f834cfca749356f017b
Ubuntu <ubuntu@ip-172-31-85-163.ec2.internal>
2018-09-13 14:37:04 +0000

    creating model_train.py

---
+++ model_train.py
@@ -0,0 +1,18 @@
+# Import packages 
+import os 
+import sys
+
+import random
+import math
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+from skimage import  measure 
+from skimage.transform import resize
+import tensorflow as tf
+from tensorflow import keras 
+import pydicom
+from imgaug import augmenters as iaa
+import pandas as pd 
+
+print(2+2)
